68c6574c8e95d5cd3210b2666e50d5649330b60f
森本竜治 <morimotoryuuji@morimotoryuujinoMacBook-Pro.local>
2019-03-08 23:50:24 +0900

    jio

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -89,7 +89,9 @@ class NewsController extends Controller
 
       // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
       if (isset($form['image'])) {
-        $path = Storage::disk('s3')->putFile('techboost',$form['image'],'public');
+        \Debugbar::info($form['image']);
+
+        $path = Storage::disk('s3')->putFile('mynews',$form['image'],'public');
         $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news->image_path = null;
